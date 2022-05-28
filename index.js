45acb10f1a618dca3714ba4f428fe570da879a58
Carlos Alberto López Mata <mata66666666666@gmail.com>
2022-05-28 16:46:39 -0500

    update

--- index.js
+++ index.js
@@ -3,19 +3,12 @@ const displayValorActual = document.getElementById('valor-actual');
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
 
-const displays = new Display(displayValorAnterior, displayValorActual);
+const display = new Display(displayValorAnterior, displayValorActual);
 
 botonesNumeros.forEach(boton => {
-  boton.addEventListener('click', () => displays.agregarNumero(boton.innerHTML));
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
 });
 
 botonesOperadores.forEach(boton => {
-  boton.addEventListener('click', () => display.computar(valor))
+    boton.addEventListener('click', () => display.computar(boton.value))
 });
-
-/*function suma (a,b) {
-  var sum = a+b;
-  document.getElementById('result').innerHTML = sum;
-};
-suma(5,5);
-*/
